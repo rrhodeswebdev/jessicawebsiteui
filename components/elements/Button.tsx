496fe8243c62c979e073a478b6995b49b2f6d13a
Ryan Rhodes <ryan@ryanrhodes.io>
2021-07-11 22:18:24 -0400

    feat: :sparkles: about page content

--- components/elements/Button.tsx
+++ components/elements/Button.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function Button({
-  children,
-  href,
+    children,
+    href,
 }: {
-  children: React.ReactNode;
-  href: string;
+    children: React.ReactNode;
+    href: string;
 }) {
-  return (
-    <Link href={href}>
-      <a className='p-4 mx-8 mb-8 md:mb-0 w-52 rounded text-white text-center bg-primary hover:bg-accent transition-colors'>
-        {children}
-      </a>
-    </Link>
-  );
+    return (
+        <Link href={href}>
+            <a className='p-4 mx-8 mb-8 md:mb-0 w-full md:w-52 rounded text-white text-center bg-primary hover:bg-accent transition-colors'>
+                {children}
+            </a>
+        </Link>
+    );
 }
