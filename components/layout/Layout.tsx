878c0f78dd620b8aef11835f46694e825ec0c25d
Ryan Rhodes <ryan@ryanrhodes.io>
2021-08-15 00:53:52 -0400

    fix(typescript): layout components

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='flex flex-col'>
-      <Navbar />
-      <main className='md:min-h-screen'>{children}</main>
-      <Footer />
-    </div>
-  );
+interface Props {
+	children: React.ReactNode;
 }
+
+const Layout: React.FC<Props> = ({ children }) => {
+	return (
+		<div className='flex flex-col'>
+			<Navbar />
+			<main className='md:min-h-screen'>{children}</main>
+			<Footer />
+		</div>
+	);
+};
+
+export default Layout;
