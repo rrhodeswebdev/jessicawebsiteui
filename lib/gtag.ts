f2e836a0c88c6bbcb750e79cc03ac997a884cf77
Ryan Rhodes <ryan@ryanrhodes.io>
2021-08-22 11:15:12 -0400

    feat(ga): added ga config

---
+++ lib/gtag.ts
@@ -0,0 +1,23 @@
+declare global {
+	interface Window {
+		gtag: any;
+	}
+}
+
+// log the pageview with their URL
+export const pageview = (url: string) => {
+	window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+		page_path: url,
+	});
+};
+
+// log specific events happening.
+export const event = ({
+	action,
+	params,
+}: {
+	action: string;
+	params: string;
+}) => {
+	window.gtag('event', action, params);
+};
