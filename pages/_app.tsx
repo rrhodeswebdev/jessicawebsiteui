21e6d7bf6547eccad949b18168f07537f992bd90
Ryan Rhodes <ryan@ryanrhodes.io>
2021-08-22 11:25:50 -0400

    feat(ga): added ga config (#57)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
-import '../styles/globals.css';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import Layout from '../components/layout/Layout';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+
+		router.events.on('routeChangeComplete', handleRouteChange);
+
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }
 export default MyApp;
