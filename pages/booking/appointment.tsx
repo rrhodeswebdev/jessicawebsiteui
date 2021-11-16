f53e34eba1679aaee899155cfd7d685453ff393c
Ryan Rhodes <ryan@ryanrhodes.io>
2021-11-16 13:03:03 -0500

    feat(seo): updated seo data and optimized images

--- pages/booking/appointment.tsx
+++ pages/booking/appointment.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import Heading from '../../components/content/Heading';
 import AppointmentForm from '../../components/forms/AppointmentForm';
+import Head from "next/head";
 
 const AppointmentPage: React.FC = () => {
-	return (
-		<section className='container mx-auto max-w-6xl px-8'>
-			<Heading title='Request Appointment' />
-			<div className='mb-8'>
-				<p className='text-center font-light'>
-					Thanks so much for your interest in booking an appointment
-					with me! Please fill out the form below and let me know
-					which service/s you are interested in! I will be in touch
-					soon to confirm your appointment. I look forward to seeing
-					you soon!
-				</p>
-			</div>
-			<div>
-				<AppointmentForm />
-			</div>
-		</section>
-	);
+    return (
+        <section className='container mx-auto max-w-6xl px-8'>
+            <Head>
+                <title>Appointments - Graced Studio</title>
+                <meta name='description'
+                      content="Request an appointment for any of my services during available time slots."/>
+            </Head>
+            <Heading title='Request Appointment'/>
+            <div className='mb-8'>
+                <p className='text-center font-light'>
+                    Thanks so much for your interest in booking an appointment
+                    with me! Please fill out the form below and let me know
+                    which service/s you are interested in! I will be in touch
+                    soon to confirm your appointment. I look forward to seeing
+                    you soon!
+                </p>
+            </div>
+            <div>
+                <AppointmentForm/>
+            </div>
+        </section>
+    );
 };
 
 export default AppointmentPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,31 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	purge: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {
-				primary: '#1a1713',
-				secondary: '#c8c2b9',
-				accent: '#b18e72',
-				shade: '#e0ddd7',
-			},
-		},
-		fontFamily: {
-			sans: ['Lato', ...defaultTheme.fontFamily.sans],
-			serif: [...defaultTheme.fontFamily.serif],
-			mono: [...defaultTheme.fontFamily.mono],
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a1713',
+        secondary: '#c8c2b9',
+        accent: '#b18e72',
+        shade: '#e0ddd7',
+      },
+      height: {
+        fs: '750px'
+      }
+    },
+    fontFamily: {
+      sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
