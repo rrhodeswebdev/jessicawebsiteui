554bed89b2c2ff464292fc8e480a3af0b012abbc
Ryan Rhodes <ryan@ryanrhodes.io>
2022-01-11 22:54:08 -0500

    chore(digitalocean): update repo for digital ocean migration (#148)
    * fix(digitalocean): :fire: removed unnecessary code for digital ocean migration
    
    * fix(vscode): :recycle: added ignore for vscode settings
    
    * fix(vscode): :fire: removed settings file
    
    * fix(vscode): :bug: fixed vscode settings in git

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head'
-import {groq} from 'next-sanity';
-import {sanityClient} from '../lib/config';
+import { groq } from 'next-sanity';
+import { sanityClient } from '../lib/config';
 import FullScreenHero from '../components/content/FullScreenHero';
 import Testimonials from '../components/content/Testimonials';
 import Button from '../components/elements/Button';
@@ -21,17 +21,17 @@ const testimonialQuery = groq`
 `;
 
 const HomePage: React.FC<Props> = props => {
-    const {data: testimonials} = props;
+    const { data: testimonials } = props;
 
     return (
         <>
             <Head>
                 <title>Home - Graced Studio</title>
                 <meta name='description'
-                      content="Graced Studio by Jesse Rhodes is Concord's premiere salon for coloring and extension services."/>
+                    content="Graced Studio by Jesse Rhodes is Concord's premiere salon for coloring and extension services." />
             </Head>
-            <FullScreenHero image={homePageImage} altText='Homepage Header'/>
-            <Testimonials testimonials={testimonials.testimonials}/>
+            <FullScreenHero image={homePageImage} altText='Homepage Header' />
+            <Testimonials testimonials={testimonials.testimonials} />
             <div className='flex flex-col md:flex-row justify-center items-center p-6 md:mb-8'>
                 <Button href='/booking/appointment'>Appointments</Button>
                 <Button href='/education'>Classes</Button>
@@ -45,7 +45,7 @@ export async function getStaticProps() {
 
     return {
         props: {
-            data: {testimonials},
+            data: { testimonials },
         },
     };
 }
